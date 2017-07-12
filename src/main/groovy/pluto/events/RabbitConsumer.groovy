e72957db9c43580093180f136fbe4e15cdf2264a
Mario Garcia <mario.ggar@gmail.com>
2017-07-12 14:22:06 +0200

    get rid of docker client in consumer constructor

--- src/main/groovy/pluto/events/RabbitConsumer.groovy
+++ src/main/groovy/pluto/events/RabbitConsumer.groovy
@@ -5,8 +5,6 @@ import groovy.util.logging.Slf4j
 import com.rabbitmq.client.DefaultConsumer
 import com.rabbitmq.client.ConnectionFactory
 
-import com.github.dockerjava.api.DockerClient
-
 /**
  * Consumes messages from the configured message queue
  *
@@ -15,16 +13,13 @@ import com.github.dockerjava.api.DockerClient
 @Slf4j
 abstract class RabbitConsumer extends DefaultConsumer {
 
-  final DockerClient client
-
   /**
    * Receives a channel to get messages from
    *
    * @since 0.1.0
    */
   @Inject
-  RabbitConsumer(ConnectionFactory factory, DockerClient client) {
+  RabbitConsumer(ConnectionFactory factory) {
     super(factory.newConnection().createChannel())
-    this.client = client
   }
 }
