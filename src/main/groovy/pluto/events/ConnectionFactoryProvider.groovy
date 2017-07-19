ee5607c348a950242d652dff70d9fbd6076d792a
Mario Garcia <mario.ggar@gmail.com>
2017-07-20 00:00:23 +0200

    add queue and exchange creation if required

--- src/main/groovy/pluto/events/ConnectionFactoryProvider.groovy
+++ src/main/groovy/pluto/events/ConnectionFactoryProvider.groovy
@@ -3,6 +3,8 @@ package pluto.events
 import javax.inject.Inject
 import javax.inject.Provider
 import pluto.config.PlutoConfig
+import com.rabbitmq.client.Channel
+import com.rabbitmq.client.Connection
 import com.rabbitmq.client.ConnectionFactory
 
 /**
@@ -29,6 +31,15 @@ class ConnectionFactoryProvider implements Provider<ConnectionFactory> {
     factory.setPassword(broker.password)
     factory.setHost(broker.host)
     factory.setPort(broker.port)
+
+    Connection connection = factory.newConnection()
+    Channel channel = connection.createChannel()
+
+    channel.exchangeDeclare(config.events.publish.exchange, "topic")
+    channel.queueDeclare(config.events.consume.queue, false, false, false, null)
+
+    channel.close()
+    connection.close()
 
     return factory
   }
