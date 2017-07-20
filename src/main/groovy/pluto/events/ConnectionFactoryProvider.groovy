98528be8a19f1d13c5652ed9d75a870d453cf0a7
Mario Garcia <mario.ggar@gmail.com>
2017-07-20 07:56:13 +0200

    add noWait to queue declaration

--- src/main/groovy/pluto/events/ConnectionFactoryProvider.groovy
+++ src/main/groovy/pluto/events/ConnectionFactoryProvider.groovy
@@ -1,5 +1,6 @@
 package pluto.events
 
+import groovy.util.logging.Slf4j
 import javax.inject.Inject
 import javax.inject.Provider
 import pluto.config.PlutoConfig
@@ -12,6 +13,7 @@ import com.rabbitmq.client.ConnectionFactory
  *
  * @since 0.1.0
  */
+@Slf4j
 class ConnectionFactoryProvider implements Provider<ConnectionFactory> {
 
   /**
@@ -26,18 +28,21 @@ class ConnectionFactoryProvider implements Provider<ConnectionFactory> {
   ConnectionFactory get() {
     PlutoConfig.Broker broker = config.events.broker
 
+    log.debug "creating connection factory"
     ConnectionFactory factory = new ConnectionFactory()
     factory.setUsername(broker.username)
     factory.setPassword(broker.password)
     factory.setHost(broker.host)
     factory.setPort(broker.port)
 
+    log.debug "creating connection and channel"
     Connection connection = factory.newConnection()
     Channel channel = connection.createChannel()
 
-    channel.exchangeDeclare(config.events.publish.exchange, "topic")
-    channel.queueDeclare(config.events.consume.queue, false, false, false, null)
+    log.debug "declaring queue"
+    channel.queueDeclareNoWait(config.events.consume.queue, false, false, false, null)
 
+    log.debug "closing channel and connection"
     channel.close()
     connection.close()
 
